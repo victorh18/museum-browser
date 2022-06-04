b9377f2f2de1e87b9d6f5e18c7478781d7f210c8
victorh18 <victorwmh18@gmail.com>
2022-06-04 19:24:35 -0400

    del(test): deleting unnecesary test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('museum-library');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('museum-library app is running!');
-  });
 });
