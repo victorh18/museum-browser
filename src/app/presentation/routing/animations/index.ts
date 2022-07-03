ee7fcd7551a17fb75fa60be519a6bc26d173b3ea
victorh18 <victorwmh18@gmail.com>
2022-07-02 20:35:21 -0400

    fix(animation): correcting animation for deployment and removing console log

--- src/app/presentation/routing/animations/index.ts
+++ src/app/presentation/routing/animations/index.ts
@@ -2,5 +2,5 @@ import { ArtworkDetailsComponent } from "../../views/artwork-details/artwork-det
 import { HomeComponent } from "../../views/home/home.component";
 
 export const FaderTransitions = [
-    `${HomeComponent.name} => ${ArtworkDetailsComponent.name}`
+    `${HomeComponent.animationId} => ${ArtworkDetailsComponent.animationId}`
 ]
--- src/app/presentation/views/home/home.component.ts
+++ src/app/presentation/views/home/home.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { MuseumService } from 'src/app/application/services/museum-service';
 import { Image } from 'src/app/core/entities/image-data';
 import { SearchParams } from 'src/app/core/entities/search-params';
+import { IView } from 'src/app/core/entities/view';
 
 @Component({
     selector: 'home',
@@ -12,18 +13,18 @@ import { SearchParams } from 'src/app/core/entities/search-params';
     ]
 })
 
-export class HomeComponent {
+export class HomeComponent extends IView {
     images: Image[] = [];
-    museumService: MuseumService
+    museumService: MuseumService;
+    public static override animationId = "Home";
 
     constructor(museumService: MuseumService, private router: Router) { 
+        super();
         this.museumService = museumService;
         this.images = this.museumService.getArtworks(1, <SearchParams>{}).map(a => ({imageId: a.id.toString(), imageUrl: a.imageUrl}) );
     }
 
     carouselClickCurrent(image: Image): void {
-        console.log("before routing");
-        
         this.router.navigate([`artworks/1/${image.imageId}`]);
     }
 
