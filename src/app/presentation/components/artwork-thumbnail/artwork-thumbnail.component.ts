bb3935b27c63b19fdc7151d3dfdb5a70ab3117a6
victorh18 <victorwmh18@gmail.com>
2022-09-30 12:37:18 -0400

    feat: adding zoom in capabilities and refactoring component data

--- src/app/presentation/components/artwork-thumbnail/artwork-thumbnail.component.ts
+++ src/app/presentation/components/artwork-thumbnail/artwork-thumbnail.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from "@angular/core";
+import { Component, ElementRef, Input, ViewChild, AfterViewInit } from "@angular/core";
+import { Artwork } from "src/app/core/entities/artwork";
 
 @Component({
     selector: 'artwork-thumbnail',
@@ -7,7 +8,20 @@ import { Component, Input } from "@angular/core";
         './artwork-thumbnail.component.css'
     ]
 })
-export class ArtworkThumbnail {
-    @Input() imageUrl: string
+export class ArtworkThumbnail implements AfterViewInit{
+    @Input() artwork: Artwork
+    @ViewChild('thumbnailContainer') thumbnailContainer: ElementRef;
 
+    ngAfterViewInit(): void {
+        const imageDiv = this.thumbnailContainer.nativeElement.children[0].children[0];
+        this.thumbnailContainer.nativeElement.addEventListener('mouseover', () => {
+            imageDiv.setAttribute('style', "transform: scale(1.1)")
+        })
+
+        this.thumbnailContainer.nativeElement.addEventListener('mouseout', () => {
+            imageDiv.setAttribute('style', "transform: scale(1)")
+
+        })
+    }
+    
 }
--- src/app/presentation/views/test-view/test-view.component.ts
+++ src/app/presentation/views/test-view/test-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { Artwork } from 'src/app/core/entities/artwork';
 import { SearchModalComponent } from '../../components/search-modal/search-modal.component';
 
 @Component({
@@ -11,7 +12,49 @@ import { SearchModalComponent } from '../../components/search-modal/search-modal
 })
 
 export class TestViewComponent implements OnInit {
-    @ViewChild('thumbnailContainer') thumbnailContainer: ElementRef;
+    @ViewChild('thumbnailListContainer') thumbnailListContainer: ElementRef;
+
+    testData: Artwork[] = [
+        {
+            id: 1,
+            internalId: '1',
+            title: 'Sexy Cupid',
+            description: "Weird representation of a Saint",
+            elaborationDate: new Date('2020-04-20'),
+            presentingDate: '2011',
+            imageUrl: 'https://lh3.ggpht.com/96neJi_Hm4RqBwlFpYlnRUnVm1Qmp6cE25QH4ouQ-a3-Mxm1Pot27J8oTkDijoeg9c1DX6rZYSF2u0nVkzZarhZ1s4dC=s0',
+            author: 'Guess some Italian gui',
+            materials: ['Paint'],
+            techniques: [ 'chromatic' ],
+            originLocation: 'a county'
+        },
+        {
+            id: 2,
+            internalId: '2',
+            title: 'American Terrorism',
+            description: "What America does to itself",
+            elaborationDate: new Date('2020-04-20'),
+            presentingDate: '2011',
+            imageUrl: 'https://i.kym-cdn.com/photos/images/original/001/382/062/819.jpg',
+            author: 'anon',
+            materials: ['Microsoft Paint'],
+            techniques: [ 'Dankness' ],
+            originLocation: 'Reddit'
+        },
+        {
+            id: 3,
+            internalId: '3',
+            title: 'Happy Shiba',
+            description: "good boi",
+            elaborationDate: new Date('2020-04-20'),
+            presentingDate: '2011',
+            imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+            author: 'Some Japanese chick',
+            materials: ['A Sony Camera'],
+            techniques: [ 'flufiness' ],
+            originLocation: 'the japanese mountains'
+        }
+    ]
 
     constructor(private dialog: MatDialog) { 
 
@@ -41,24 +84,16 @@ export class TestViewComponent implements OnInit {
 
     setHeightValue(): void {
         const windowWidth = window.innerWidth;
-        const childrenLength = this.thumbnailContainer.nativeElement.children.length;
+        const childrenLength = this.thumbnailListContainer.nativeElement.children.length;
         const columnCount = windowWidth > 550 ? 3 : 2;
         const columnClearance = columnCount * 10
-
-        // const logValues = {
-        //     thumbnailHeight: this.thumbnailContainer.nativeElement.children[1].offsetHeight,
-        //     windowWidth,
-        //     childrenLength: this.thumbnailContainer.nativeElement.children.length
-        // }
-
-        //console.table(logValues);
 
         const colWidth = (windowWidth - columnClearance) / columnCount;
         
         const positionArray = [...new Array(columnCount)].map(() => { return 20 });
 
         for (let index = 0; index < childrenLength; index++) {
-            const element = this.thumbnailContainer.nativeElement.children[index].children[0];
+            const element = this.thumbnailListContainer.nativeElement.children[index].children[0];
             //console.log("affected element: ", element);
 
             const colPosition = index % columnCount;
